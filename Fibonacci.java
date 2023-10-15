30bb12120ee3ad7838d7d3a6a0406a39fe210cf2
Bhavya_Vadavalli <90544123+BhavyaVadavalli@users.noreply.github.com>
2023-10-16 00:40:18 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,20 +1,16 @@
 
 
 class Fibanocci {
-
 	
 	static void Fibonacci(int N)
 	{
-		int num1 = 1, num2 = 3;
+		int num1 = 2, num2 = 3;
 
 		int counter = 0;
 
-
 		while (counter < N) {
 
-	
 			System.out.print(num1 + " ");
-
 			
 			int num3 = num2 + num1;
 			num1 = num2;
@@ -23,12 +19,10 @@ class Fibanocci {
 		}
 	}
 
-
 	public static void main(String args[])
 	{
 		
 		int N = 5;
-
 	
 		Fibonacci(N);
 	}
